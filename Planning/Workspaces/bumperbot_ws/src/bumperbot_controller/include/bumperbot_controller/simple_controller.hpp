5756bd471abfc593e28d5963e874b294f9d0f945
HanielUlises <hanielulises2003@gmail.com>
2025-11-20 14:26:28 -0600

    Simple kinematics

---
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -0,0 +1,24 @@
+#ifndef SIMPLE_CONTROLLER_HPP
+#define SIMPLE_CONTROLLER_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+#include <Eigen/Core>
+
+class SimpleController : public rclcpp::Node{
+    public:
+        SimpleController(const std::string &name);
+
+    private: 
+        void velocity_callback(const geometry_msgs::msg::TwistStamped &msg);
+
+        rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub;
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub;
+
+        double wheel_radius;
+        double wheel_separation;
+        Eigen::Matrix2d speed_conversion;
+};
+
+#endif
