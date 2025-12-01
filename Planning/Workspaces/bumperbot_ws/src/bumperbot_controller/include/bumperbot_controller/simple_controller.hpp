75ac7c3db119ec296281799e164baac77fd1cd73
HanielUlises <hanielulises2003@gmail.com>
2025-12-01 17:10:05 -0600

    Differential inverse kinematic

--- Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -4,6 +4,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 #include <Eigen/Core>
 
 class SimpleController : public rclcpp::Node{
@@ -12,13 +13,20 @@ class SimpleController : public rclcpp::Node{
 
     private: 
         void velocity_callback(const geometry_msgs::msg::TwistStamped &msg);
+        void joint_callback(const sensor_msgs::msg::JointState &msg);
 
         rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub;
         rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub;
+        rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub;
 
         double wheel_radius;
         double wheel_separation;
         Eigen::Matrix2d speed_conversion;
+        
+        // Positions for odometry
+        double left_wheel_prev_pos;
+        double right_wheel_prev_pos;
+        rclcpp::Time previous_time;
 };
 
 #endif
--- Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
@@ -4,8 +4,8 @@
 
 using std::placeholders::_1;
 
-SimpleController::SimpleController(const std::string &name) 
-    : Node(name){
+SimpleController::SimpleController(const std::string &name) : Node(name), 
+    left_wheel_prev_pos(0.0), right_wheel_prev_pos(0.0) {
     declare_parameter("wheel_radius", 0.033);
     declare_parameter("wheel_separation", 0.17);
 
@@ -15,9 +15,14 @@ SimpleController::SimpleController(const std::string &name)
     RCLCPP_INFO_STREAM(get_logger(), "Using wheel_radius: " << wheel_radius);
     RCLCPP_INFO_STREAM(get_logger(), "Using wheel_separation: " << wheel_separation);
 
+    previous_time = get_clock() -> now();
+
     wheel_cmd_pub = create_publisher<std_msgs::msg::Float64MultiArray>("/simple_velocity_controller/commands", 10);
     vel_sub = create_subscription<geometry_msgs::msg::TwistStamped>("/bumperbot_controller/cmd_vel", 
         10, std::bind(&SimpleController::velocity_callback, this, _1));
+
+    joint_sub = create_subscription<sensor_msgs::msg::JointState>("/joint_states",
+        10, std::bind(&SimpleController::joint_callback, this, _1));
 
     speed_conversion << wheel_radius/2, wheel_radius/2, wheel_radius/wheel_separation, -wheel_radius/wheel_separation;  
 
@@ -35,6 +40,27 @@ void SimpleController::velocity_callback(const geometry_msgs::msg::TwistStamped 
     
     wheel_cmd_pub -> publish(wheel_speed_msg);
 
+}
+
+void SimpleController::joint_callback(const sensor_msgs::msg::JointState &msg) {
+    double delta_right = msg.position.at(0) - right_wheel_prev_pos;
+    double delta_left = msg.position.at(1) - left_wheel_prev_pos;
+    
+    rclcpp::Time msg_time = msg.header.stamp;
+    rclcpp::Duration dt = msg_time - previous_time;
+
+    right_wheel_prev_pos = msg.position.at(0);
+    left_wheel_prev_pos = msg.position.at(1);
+
+    previous_time = msg_time;
+
+    double fi_right = delta_right / dt.seconds();
+    double fi_left = delta_left / dt.seconds();
+
+    double linear = (wheel_radius * fi_right + wheel_radius * fi_left) / 2;
+    double angular = (wheel_radius * fi_right - wheel_radius * fi_left) / wheel_separation;
+
+    RCLCPP_INFO_STREAM(get_logger(), "Linear velocity: " << linear << "\n" <<"Angular Velocity" << angular);
 }
 
 int main(int argc, char* argv[]){
