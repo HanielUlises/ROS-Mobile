3b5bb77d65538c9e4415434dc8418743c30fc37c
HanielUlises <hanielulises2003@gmail.com>
2026-03-24 23:13:05 -0600

    Filter initialization

---
+++ Planning/Workspaces/bumperbot_ws/src/bumpberbot_localization/include/bumpberbot_localization/kalman_filter.hpp
@@ -0,0 +1,25 @@
+#ifndef KALMAN_FILTER_HPP
+#define KALMAN_FILTER_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+
+class KalmanFilter : public rclcpp::Node {
+    public:
+        KalmanFilter(const std::string &name);
+    private:
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+        rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;
+
+        double mean;
+        double variance;
+        double imu_angular_z;
+        double last_angular_z_;
+
+        bool is_first_odom_;
+
+};
+
+#endif
