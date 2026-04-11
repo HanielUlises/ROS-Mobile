2ac42ca5d12ccc0b69832654a5a49f55d555a925
HanielUlises <hanielulises2003@gmail.com>
2026-04-10 23:49:12 -0600

    Controller twist relay

---
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_controller/src/twist_relay.cpp
@@ -0,0 +1,14 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+
+class TwistRelay : public rclcpp::Node {
+    public:
+        TwistRelay() : Node("twist_relay") {
+
+        }
+    
+    private:
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr controller_sub;
+        rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr controller_pub;
+};
