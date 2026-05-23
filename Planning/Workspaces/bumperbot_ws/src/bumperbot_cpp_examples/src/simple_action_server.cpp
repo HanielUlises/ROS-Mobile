6a283bb07df85c57743483739e029ca99037e6ba
HanielUlises <hanielulises2003@gmail.com>
2026-05-22 23:36:03 -0600

    Action server dispatch

--- Planning/Workspaces/bumperbot_ws/src/bumperbot_cpp_examples/src/simple_action_server.cpp
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_cpp_examples/src/simple_action_server.cpp
@@ -1,12 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
 namespace bumperbot_cpp_examples {
     class SimpleActionServer : public rclcpp::Node {
         public:
-            explicit SimpleActionServer(const rclcpp::NodeOptions &options = rclcpp::NodeOptions()) : Node("simple_action_server") {
+            explicit SimpleActionServer(const rclcpp::NodeOptions &options = rclcpp::NodeOptions()) : Node("simple_action_server", options) {
 
             }
 
         private:
+            rclcpp_action::Server<>::SharedPtr action_server_;
     };
 }
