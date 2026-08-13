6ad3be9bc4e093dc8ebd8fdb7139891b90956953
HanielUlises <hanielulises2003@gmail.com>
2026-08-12 23:31:31 -0600

    Mapping with known poses

---
+++ Planning/Workspaces/bumperbot_ws/src/bumperbot_mapping/include/bumperbot_mapping/mapping_with_known_poses.hpp
@@ -0,0 +1,13 @@
+#ifndef MAPPING_WITH_KNOWN_POSES
+#define MAPPING_WITH_KNOWN_POSES
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+namespace bumperbot_mapping {
+    class MappinWithKnownPoses : public rclcpp::Node {
+
+    }
+}
+
+#endif
